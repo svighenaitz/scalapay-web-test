fff8c6bf3361360db89258baef7412e576565a1e
Giuliano De Ioanni <giulianodeioannigcp@gmail.com>
2025-05-19 09:34:53 +0200

    feat: base test and coverage

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -3,11 +3,13 @@ import { render, screen } from '@testing-library/react'
 import Page from '@/pages/index'
  
 describe('Page', () => {
-  it('renders a heading', () => {
+  it('renders a heading and a link', () => {
     render(<Page />)
- 
+
     const heading = screen.getByRole('heading', { level: 1 })
- 
     expect(heading).toBeInTheDocument()
+
+    // Check for the "START FILLING FORM" link text
+    expect(screen.getByText('START FILLING FORM')).toBeInTheDocument()
   })
 })
--- jest.config.ts
+++ jest.config.ts
@@ -10,8 +10,12 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: 'v8',
   testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  collectCoverageFrom: [
+    // 'components/**/*.{ts,tsx}',
+    'pages/**/*.{ts,tsx}',
+    // '!**/*.d.ts'
+  ],
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
