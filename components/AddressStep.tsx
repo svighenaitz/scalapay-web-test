aa5112e8965aaef92f7c62e6a5d703bf9a0d4583
Giuliano De Ioanni <giulianodeioannigcp@gmail.com>
2025-05-20 13:14:37 +0200

    styling: page step 2

--- components/AddressStep.tsx
+++ components/AddressStep.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useFormStore } from '../store/formSlice';
 import styles from './AddressStep.module.css';
+import accountStepStyles from './AccountStep.module.css';
 import FormStep from './FormStep';
 import FormInput from './FormInput';
 import FormButton from './FormButton';
 import { useFormFieldChange } from '../hooks/useFormFieldChange';
 
 const countryOptions = [
-  { value: '', label: 'Seleziona Nazione' },
+  { value: '', label: 'Nazione' },
   { value: 'IT', label: 'Italia' },
   { value: 'ES', label: 'Spagna' },
   { value: 'DE', label: 'Germania' },
@@ -22,106 +23,108 @@ const AddressStep: React.FC<AddressStepProps> = ({ loading = false }) => {
   const handleChange = useFormFieldChange(updateAddress, errors, setError);
 
   return (
-    <FormStep heading="Indirizzo di residenza" className={styles.container}>
-      <div className={styles.flex}>
-        <FormInput
-          type="text"
-          name="address"
-          placeholder="Via, piazza, etc"
-          value={address.address}
-          onChange={handleChange}
-          errorName="address"
-          className={styles.inputDouble}
-          data-testid="address-address-input"
-        />
-        <FormInput
-          type="number"
-          name="addressNumber"
-          placeholder="N°"
-          value={address.addressNumber}
-          onChange={handleChange}
-          errorName="addressNumber"
-          className={styles.inputHalf}
-          min={1}
-          data-testid="address-addressNumber-input"
-        />
-      </div>
-      <div className={styles.flex}>
-        <FormInput
-          type="text"
-          name="postalCode"
-          placeholder="CAP"
-          value={address.postalCode}
-          onChange={handleChange}
-          errorName="postalCode"
-          className={styles.inputHalf}
-          data-testid="address-postalCode-input"
-        />
-        <FormInput
-          type="text"
-          name="province"
-          placeholder="Provincia"
-          value={address.province}
-          onChange={handleChange}
-          errorName="province"
-          className={styles.inputHalf}
-          data-testid="address-province-input"
-        />
-      </div>
-      <div className={styles.flex}>
-        <FormInput
-          type="text"
-          name="city"
-          placeholder="Città"
-          value={address.city}
-          onChange={handleChange}
-          errorName="city"
-          className={styles.inputHalf}
-          data-testid="address-city-input"
-        />
-        <FormInput
-          as="select"
-          name="country"
-          value={address.country}
-          onChange={handleChange}
-          errorName="country"
-          className={styles.inputHalf}
-          options={countryOptions}
-          data-testid="address-country-input"
-        />
-      </div>
-      <div className={styles.flexAlignCenter}>
-        <label className={styles.label} htmlFor="currentlyLiveHere">
-          <FormInput
-            id="currentlyLiveHere"
-            type="checkbox"
-            name="currentlyLiveHere"
-            checked={address.currentlyLiveHere}
-            onChange={handleChange}
-            errorName="currentlyLiveHere"
-          />
-          I currently live here
-        </label>
-        <label className={styles.label} htmlFor="isPEP">
-          <FormInput
-            id="isPEP"
-            type="checkbox"
-            name="isPEP"
-            checked={address.isPEP}
-            onChange={handleChange}
-            errorName="isPEP"
-          />
-          Dichiaro di essere una PEP
-        </label>
-      </div>
+    <>
+      <FormStep heading="Indirizzo di residenza" className={styles.container}>
+        <div className={accountStepStyles.formInputsColumn} >
+          <div className={styles.flexLeft}>
+            <FormInput
+              type="text"
+              name="address"
+              placeholder="Via, piazza, etc"
+              value={address.address}
+              onChange={handleChange}
+              errorName="address"
+              className={styles.inputFull}
+              data-testid="address-address-input"
+            />
+            <FormInput
+              type="number"
+              name="addressNumber"
+              placeholder="N°"
+              value={address.addressNumber}
+              onChange={handleChange}
+              errorName="addressNumber"
+              className={styles.inputFull}
+              min={1}
+              data-testid="address-addressNumber-input"
+            />
+          </div>
+          <div className={styles.flexRight}>
+            <FormInput
+              type="text"
+              name="postalCode"
+              placeholder="CAP"
+              value={address.postalCode}
+              onChange={handleChange}
+              errorName="postalCode"
+              className={styles.inputFull}
+              data-testid="address-postalCode-input"
+            />
+            <FormInput
+              type="text"
+              name="province"
+              placeholder="Provincia"
+              value={address.province}
+              onChange={handleChange}
+              errorName="province"
+              className={styles.inputFull}
+              data-testid="address-province-input"
+            />
+          </div>
+            <FormInput
+              type="text"
+              name="city"
+              placeholder="Città"
+              value={address.city}
+              onChange={handleChange}
+              errorName="city"
+              className={styles.inputFull}
+              data-testid="address-city-input"
+            />
+            <FormInput
+              as="select"
+              name="country"
+              value={address.country}
+              onChange={handleChange}
+              errorName="country"
+              className={styles.inputFull}
+              options={countryOptions}
+              data-testid="address-country-input"
+            />
+          <div className={styles.flexAlignCenter}>
+            <label className={styles.label} htmlFor="currentlyLiveHere">
+              <FormInput
+                id="currentlyLiveHere"
+                type="checkbox"
+                name="currentlyLiveHere"
+                checked={address.currentlyLiveHere}
+                onChange={handleChange}
+                errorName="currentlyLiveHere"
+              />
+              I currently live here
+            </label>
+            <label className={styles.label} htmlFor="isPEP">
+              <FormInput
+                id="isPEP"
+                type="checkbox"
+                name="isPEP"
+                checked={address.isPEP}
+                onChange={handleChange}
+                errorName="isPEP"
+              />
+              Dichiaro di essere una PEP
+            </label>
+          </div>
+        </div>
+      </FormStep>
       <FormButton
         loading={loading}
         label="Salva"
         loadingLabel="Attendi..."
-        className={styles.button}
+        className={accountStepStyles.bottomButton}
         data-testid="address-continue-button"
       />
-    </FormStep>
+    </>
   );
 };
 
